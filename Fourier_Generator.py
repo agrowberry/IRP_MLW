f39d6285df853d2bdc22bd79cfc508d62b671057
agrowberry <archer.rowberry@gmail.com>
2021-02-16 16:42:42 +0000

    slight update fleshing out fourier generator module.

--- Fourier_Generator.py
+++ Fourier_Generator.py
@@ -44,6 +44,74 @@ class FourierManipulation(object):
     def __init__(self):
         self.fig = go.Figure()
         self.geom_dict = {}
+        self.point_array = np.array(np.zeros((3, 1000)))
+        self.local_spiral = np.array(np.zeros(3, 100))
+        self.main_spiral = np.array(np.zeros(3, 100))
+        self.grad_array = np.array(np.zeros(3, 100))
+
+    def make_rectangle(self, width, height, length, n, output=True, return_nd_array=False):
+        """
+        returns rectangular helix of specified dimensions in array.
+        :param width:
+        :param height:
+        :param length:
+        :param output: conditional dictating whether or not method is static and outputs array, or keeps attribute
+         within class.
+        :param return_nd_array: conditional for return format.
+        :param n: no. of points in helix.
+        :return: x, y, z || or 3d array of these variables.
+        """
+        corners = [(width / 2, 0),
+                   (width / 2, height / 2),
+                   (-width / 2, height / 2),
+                   (-width / 2, -height / 2),
+                   (width / 2, -height / 2),
+                   (width / 2, 0)]
+        x_list = []
+        y_list = []
+        perimeter = 2 * width + 2 * height
+
+        magnitude = lambda p1, p2: ((p1[0] - p2[0])**2 + (p1[1] - p2[2])**2)**0.5
+
+        for i in range(1, len(corners)):
+            corner = corners[i]
+            last_corner = corners[i - 1]
+            x_points = np.linspace(last_corner[0], corner[0], int(magnitude(corner, last_corner) * n / perimeter))
+            x_list.append(x_points)
+            y_points = np.linspace(last_corner[1], corner[1], int(magnitude(corner, last_corner) * n / perimeter))
+            y_list.append(y_points)
+
+        x = np.concatenate(x_list, axis=0)
+        y = np.concatenate(y_list, axis=0)
+        z = np.linspace(0, length, y.shape[0])
+
+        if output:
+            if return_nd_array:
+                return np.array(x, y, z)
+            else:
+                return x, y, z
+        else:
+            self.local_spiral = np.array(x, y, z)
+
+    def make_helix(self, single_spiral, no_of_turns, output=False):
+
+        helix_list = [[], [], []]
+        length = single_spiral[2][-1]
+
+        for i in range(no_of_turns):
+            nth_spiral = np.copy(single_spiral)
+            nth_spiral[2] = single_spiral[2] + i * length
+            for j in range(3):
+                helix_list[j].append(nth_spiral[j])
+
+            helix_array = np.array([np.concatenate(helix_list[0]),
+                                    np.concatenate(helix_list[1]),
+                                    np.concatenate(helix_list[2])])
+
+        if output:
+            return helix_array
+        else:
+            self.main_spiral = np.copy(helix_array)
 
     def grad(self, vector_array, normalise=True):
         """
@@ -55,6 +123,7 @@ class FourierManipulation(object):
         """
         count = 1
         grad_array = []
+        last_i = 0
         for i in vector_array:
             if count == 1:
                 last_i = i
@@ -75,8 +144,41 @@ class FourierManipulation(object):
 
         return grad_array
 
+    def map_points(self, unmapped_local_array, normal_array, trans_array):
+        """
+        will map point from global origin [0, 0, 0] to translated and rotated points
+        :param unmapped_local_array: n-dimensional array of points to be mapped.
+        :param normal_array: n-dimensional array of normals to rotation point.
+        :param trans_array: n-dimensional array of translated origins.
+        :return: mapped_array: n-dimensional array of mapped points.
+        """
+
+        origin_normal = np.array([0, 1, 0])
+        points_df = pd.DataFrame(normal_array, columns=['normal'])
+        points_df['unmapped'] = unmapped_local_array
+        points_df['translation'] = trans_array
+        points_df['mapped'] = np.array(np.zeros(unmapped_local_array.shape()))
+
+        for index, row in points_df.iterrows():
+            normal_vector = row['normal']
+            c = np.dot(origin_normal, normal_vector)
+            axis = np.unitcross(origin_normal, normal_vector)
+            s = np.sqrt(1 - c**2)
+            C = 1 - c
+            x = axis[0]
+            y = axis[1]
+            z = axis[2]
+            rot_matrix = np.array([[x*x*C+c, x*y*C-z*s, x*z*C+y*s],
+                                   [y*x*C+z*s, y*y*C+c, y*z*C-x*s],
+                                   [z*x*C-y*s, z*y*C+x*s, z*z*C+c]])
+
+            points_df['mapped'][index] = np.dot(rot_matrix, row['unmapped']) + row['translation']
+
+        return pd.array(points_df['mapped'])
+
     def make_coil(self, geom_dict, N):
         """
+
         :param: geom_dict, dictionary in std_input_dict format with geometry params.
         :param: N, no. of points in returned array.
         :return: array of points describing square coil around rectangular core.
@@ -89,5 +191,12 @@ class FourierManipulation(object):
         val_list = []
         for val in geometry_dict:
             val_list.append(geometry_dict[val])
+
+
+fg = FourierManipulation()
+
+main_spiral = fg.make_rectangle()
+
+
 
 
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # IRP_MLW
 Individual Research Project | Machine Learning Winding
+
+This project provides several tools for programmatically designing, evaluating and optimising single-slot electrical windings.
+
+---
+ ## Fourier Series Module (Fourier_Generator.py)
+
+Provides all of the tools for constructing basic coil winding geometries. With many 
