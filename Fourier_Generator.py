052228eb5a266858163a0b0f2ce133e32ef483ef
agrowberry <archer.rowberry@gmail.com>
2021-02-17 19:00:10 +0000

    slight update fleshing out fourier generator module.

--- Fourier_Generator.py
+++ Fourier_Generator.py
@@ -6,24 +6,23 @@ import pandas as pd
 
 class DataEntry(object):
     def __init__(self):
-        self.std_inputs_dict = {'core_length': 0.0,
-                                'core_minor_axis': 0.0,
-                                'core_major_axis': 0.0,
-                                'num_of_turns': 0.0,
-                                'outer_minor_axis': 0.0,
-                                'outer_major_axis': 0.0,
-                                'spacing': 0.0
+        self.std_inputs_dict = {'core_length': 10.0,
+                                'core_minor_axis': 10.0,
+                                'core_major_axis': 20.0,
+                                'num_of_turns': 10.0,
+                                'outer_spacing': 3.0,
+                                'spacing': 0.5
                                 }
-
         self.yes_answer = ['y', 'Y', 'Yes', 'YES', 'yes']
         self.no_answer = ['n', 'N', 'no', 'NO', 'No']
 
     def input_data(self, input_dict=False):
         if not input_dict:
             input_dict = self.std_inputs_dict
-
-        for key in input_dict:
-            input_dict[key] = float(input('Input ' + str(key) + ': '))
+        else:
+            input_dict = self.std_inputs_dict
+            for key in input_dict:
+                input_dict[key] = float(input('Input ' + str(key) + ': '))
 
         return input_dict
 
@@ -39,22 +38,41 @@ class DataEntry(object):
     def insert_model(self, file):
         pass
 
+    def start_up(self):
+        running = True
+        while running:
+            pass_manually = input('pass dimensions manually? (y/n): ')
+            if pass_manually in self.yes_answer:
+                input_dict = self.input_data(input_dict=True)
+                running = False
+                return input_dict
+            elif pass_manually in self.no_answer:
+                input_dict = self.std_inputs_dict
+                # filename = input('enter filepath of coil dimensions: ')
+                # input_dict = self.read_data(filename)
+                running = False
+                return input_dict
+            else:
+                pass
+
 
 class FourierManipulation(object):
     def __init__(self):
         self.fig = go.Figure()
         self.geom_dict = {}
-        self.point_array = np.array(np.zeros((3, 1000)))
-        self.local_spiral = np.array(np.zeros(3, 100))
-        self.main_spiral = np.array(np.zeros(3, 100))
-        self.grad_array = np.array(np.zeros(3, 100))
+        self.point_array = np.zeros((3, 1000))
+        self.local_spiral = np.zeros((3, 1000))
+        self.main_spiral = np.zeros((3, 1000))
+        self.grad_array = np.zeros((3, 1000))
+        self.coil_sing_spiral = np.zeros((3, 1000))
+        self.coil_spiral = np.zeros((3, 1000))
 
-    def make_rectangle(self, width, height, length, n, output=True, return_nd_array=False):
+    def make_rectangle(self, width, height, length, n, output=False, return_nd_array=False):
         """
         returns rectangular helix of specified dimensions in array.
-        :param width:
-        :param height:
-        :param length:
+        :param width: width of rectangle
+        :param height:height of rectangle
+        :param length:length of coil revolution
         :param output: conditional dictating whether or not method is static and outputs array, or keeps attribute
          within class.
         :param return_nd_array: conditional for return format.
@@ -70,50 +88,54 @@ class FourierManipulation(object):
         x_list = []
         y_list = []
         perimeter = 2 * width + 2 * height
-
-        magnitude = lambda p1, p2: ((p1[0] - p2[0])**2 + (p1[1] - p2[2])**2)**0.5
-
+        magnitude = lambda p1, p2: ((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)**0.5
         for i in range(1, len(corners)):
             corner = corners[i]
             last_corner = corners[i - 1]
-            x_points = np.linspace(last_corner[0], corner[0], int(magnitude(corner, last_corner) * n / perimeter))
+            x_points = np.linspace(last_corner[0],
+                                   corner[0], int(round(magnitude(corner, last_corner) * n / perimeter)))
             x_list.append(x_points)
-            y_points = np.linspace(last_corner[1], corner[1], int(magnitude(corner, last_corner) * n / perimeter))
+            y_points = np.linspace(last_corner[1],
+                                   corner[1], int(round(magnitude(corner, last_corner) * n / perimeter)))
             y_list.append(y_points)
-
         x = np.concatenate(x_list, axis=0)
         y = np.concatenate(y_list, axis=0)
-        z = np.linspace(0, length, y.shape[0])
 
+        # fix length discrepancy caused by rounding error.
+        if x.shape[0] != n:
+            diff_len = n - x.shape[0]
+            x = np.append(x, np.full(int(diff_len), x[-1]))
+            y = np.append(y, np.full(int(diff_len), y[-1]))
+
+        z = np.linspace(0, length, y.shape[0])
         if output:
             if return_nd_array:
-                return np.array(x, y, z)
+                return np.array([x, y, z])
             else:
                 return x, y, z
         else:
-            self.local_spiral = np.array(x, y, z)
+            self.local_spiral = np.array([x, y, z])
 
     def make_helix(self, single_spiral, no_of_turns, output=False):
-
         helix_list = [[], [], []]
+        helix_array = np.zeros(single_spiral.shape)
         length = single_spiral[2][-1]
-
         for i in range(no_of_turns):
             nth_spiral = np.copy(single_spiral)
             nth_spiral[2] = single_spiral[2] + i * length
             for j in range(3):
                 helix_list[j].append(nth_spiral[j])
-
             helix_array = np.array([np.concatenate(helix_list[0]),
                                     np.concatenate(helix_list[1]),
                                     np.concatenate(helix_list[2])])
-
+            print('building spiral ' + str(i) + ' of ' + str(no_of_turns) + ' : ' + str(100 * i / no_of_turns) + '%',
+                  end='\r')
         if output:
             return helix_array
         else:
             self.main_spiral = np.copy(helix_array)
 
-    def grad(self, vector_array, normalise=True):
+    def grad(self, vector_array, normalise=True, output=False):
         """
         returns either un-normalised or normalised gradient array of an n-dimension array.
         :param vector_array: n-dimensional array of sequential points of
@@ -142,9 +164,12 @@ class FourierManipulation(object):
                 norm_grad_list.append(norm_vector)
             grad_array = np.stack(norm_grad_list)
 
-        return grad_array
+        if output:
+            return grad_array
+        else:
+            self.grad_array = np.copy(grad_array)
 
-    def map_points(self, unmapped_local_array, normal_array, trans_array):
+    def map_points(self, unmapped_local_array, normal_array, trans_array, output=False):
         """
         will map point from global origin [0, 0, 0] to translated and rotated points
         :param unmapped_local_array: n-dimensional array of points to be mapped.
@@ -152,18 +177,17 @@ class FourierManipulation(object):
         :param trans_array: n-dimensional array of translated origins.
         :return: mapped_array: n-dimensional array of mapped points.
         """
-
-        origin_normal = np.array([0, 1, 0])
-        points_df = pd.DataFrame(normal_array, columns=['normal'])
-        points_df['unmapped'] = unmapped_local_array
-        points_df['translation'] = trans_array
-        points_df['mapped'] = np.array(np.zeros(unmapped_local_array.shape()))
-
-        for index, row in points_df.iterrows():
-            normal_vector = row['normal']
+        origin_normal = np.array([0, 0, 1])
+        mapped_list = []
+        for i in range(unmapped_local_array.shape[0]):
+            normal_vector = normal_array[i]
+            unmapped_vector = unmapped_local_array[i]
+            trans_vector = trans_array[i]
             c = np.dot(origin_normal, normal_vector)
-            axis = np.unitcross(origin_normal, normal_vector)
-            s = np.sqrt(1 - c**2)
+            unnorm_axis = np.cross(origin_normal, normal_vector)
+            det_unnorm_axis = (unnorm_axis[0]**2 + unnorm_axis[1]**2 + unnorm_axis[2]**2)**0.5
+            axis = unnorm_axis * (1 / det_unnorm_axis)
+            s = np.sqrt(1 - c ** 2)
             C = 1 - c
             x = axis[0]
             y = axis[1]
@@ -171,20 +195,48 @@ class FourierManipulation(object):
             rot_matrix = np.array([[x*x*C+c, x*y*C-z*s, x*z*C+y*s],
                                    [y*x*C+z*s, y*y*C+c, y*z*C-x*s],
                                    [z*x*C-y*s, z*y*C+x*s, z*z*C+c]])
+            mapped_vector = np.dot(rot_matrix, unmapped_vector) + trans_vector
+            mapped_list.append(mapped_vector)
+            print('mapping points ' + str(i) + ' of ' + str(unmapped_local_array.shape[0]), end='\r')
+        if output:
+            return np.array(mapped_list)
+        else:
+            self.point_array = np.transpose(np.array(mapped_list))
 
-            points_df['mapped'][index] = np.dot(rot_matrix, row['unmapped']) + row['translation']
-
-        return pd.array(points_df['mapped'])
-
-    def make_coil(self, geom_dict, N):
+    def make_coil(self, N, geom_dict=True, plot=False):
         """
-
         :param: geom_dict, dictionary in std_input_dict format with geometry params.
         :param: N, no. of points in returned array.
         :return: array of points describing square coil around rectangular core.
         """
+        de_local = DataEntry()
+        if geom_dict:
+            geom_dict = de_local.start_up()
 
-        geom_df = pd.Dataframe()
+        num_turns = int(geom_dict['num_of_turns'])
+        coil_height = (geom_dict['core_length'] - (num_turns - 1)*geom_dict['spacing'])/num_turns
+        coil_width = geom_dict['outer_spacing'] - 2*geom_dict['spacing']
+        major_width = geom_dict['core_major_axis'] + geom_dict['spacing'] + coil_width/2
+        major_height = geom_dict['core_minor_axis'] +geom_dict['spacing'] + coil_height/2
+        major_length = geom_dict['core_length']/num_turns
+
+        self.make_rectangle(major_width, major_height, major_length, int(round(N/num_turns)))
+        self.make_helix(self.local_spiral, num_turns)
+        self.grad(self.main_spiral)
+
+        points_per_coil_spiral = 20
+        self.coil_sing_spiral = self.make_rectangle(coil_width, coil_height, 0, points_per_coil_spiral,
+                                                    output=True, return_nd_array=True)
+        self.coil_spiral = self.make_helix(self.coil_sing_spiral, int(round(N/points_per_coil_spiral)), output=True)
+
+        self.map_points(np.transpose(self.coil_spiral), np.transpose(self.grad_array), np.transpose(self.main_spiral))
+
+        if plot:
+            self.fig.add_trace(go.Scatter3d(x=self.point_array[0], y=self.point_array[1], z=self.point_array[2],
+                                            mode='lines'))
+            self.fig.add_trace(go.Scatter3d(x=self.main_spiral[0], y=self.main_spiral[1], z=self.main_spiral[2]))
+            self.fig.add_trace(go.Scatter3d(x=self.coil_spiral[0], y=self.coil_spiral[1], z=self.coil_spiral[2]))
+            self.fig.show()
 
     # returns array with fourier series coefficients.
     def make_fourier_series(self, geometry_dict, plot_result=True):
@@ -195,8 +247,5 @@ class FourierManipulation(object):
 
 fg = FourierManipulation()
 
-main_spiral = fg.make_rectangle()
-
-
-
+fg.make_coil(50000, plot=True)
 
