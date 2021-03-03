7dc6d1da547521b4da93097a195b46ddfa30f385
agrowberry <archer.rowberry@gmail.com>
2021-03-03 13:19:31 +0000

    Added coil_figure.html figure and coil_array_points.json

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ fm = fouriergenerator.FourierManipulation()
 
 gm.make_coil(50000, plot=True)
 
-gm.fig.write_html('coil_figure.html')
+gm.fig.write_html('coil_figure.html', include_plotlyjs='cdn')
 
 
 
