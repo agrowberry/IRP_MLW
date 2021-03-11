27aa5f487cd0be364629cec597502a9bedec05df
agrowberry <archer.rowberry@gmail.com>
2021-03-11 11:53:37 +0000

    Adding graphs for readme

--- README.md
+++ README.md
@@ -4,10 +4,11 @@ Individual Research Project | Machine Learning Winding
 This project provides several tools for programmatically designing, evaluating and optimising single-slot electrical windings.
 
 ---
- ## Fourier Series Module (Fourier_Generator.py)
+ ## Fourier Series Module (fouriergenerator.py)
 
 Provides a set of tools for constructing basic coil winding geometries. With further tools for parameterising the construction. 
 
-Also provides means for constructing the fourier transformed representation.
+The two main objects in the module are .point_array and .fft_points_array:
+- .point_array exists in the real space and is a numpy 3-dimensional array of points describing the winding coil.
 
-![](./coil_figure.html)
+- .fft_points_array is the frequency domain counterpart to .point_array. It represents the three fast fourier transformed coefficients
--- main.py
+++ main.py
@@ -3,9 +3,30 @@ import fouriergenerator
 import numpy as np
 import plotly.graph_objects as go
 import scipy as sp
+import open3d as o3d
 
 gm = fouriergenerator.GeometryManipulation()
 fm = fouriergenerator.FourierManipulation()
 
 gm.make_coil(50000, plot=True)
 
+# pc_array = np.transpose(datahandling.fetch_coil_points())
+
+
+# pcd = o3d.geometry.PointCloud()
+# pcd.points = o3d.utility.Vector3dVector(pc_array)
+# pcd.estimate_normals(
+#     search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.01, max_nn=30)
+# )
+# pcd = pcd.voxel_down_sample(voxel_size=0.05)
+# # distances = pcd.compute_nearest_neighbor_distance()
+# # avg_dist = np.mean(distances)
+# # radius = 1 * avg_dist
+# #
+# # bpa_mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(pcd,o3d.utility.DoubleVector([radius,
+# #                                                                                                          radius * 2]))
+#
+# o3d.visualization.draw_geometries([pcd])
+#
+#
+#
